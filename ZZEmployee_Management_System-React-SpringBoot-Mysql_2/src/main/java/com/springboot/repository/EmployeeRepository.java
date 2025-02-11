b581ba5319f81930f062c611f579a2c5b177f59a
freedemo1 <57246688+freedemo1@users.noreply.github.com>
2025-02-11 09:06:09 +0530

    Delete ZZEmployee_Management_System-React-SpringBoot-Mysql_2 directory
    deleted

--- ZZEmployee_Management_System-React-SpringBoot-Mysql_2/src/main/java/com/springboot/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	
-}
